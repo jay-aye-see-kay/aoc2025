666b69c1d06e65b6888101e07e01f294088e2dc1
Jack Rose <user@jackrose.co.nz>
2025-12-01 21:37:05 +1100

    day 1 part 2 rough

--- all_test.go
+++ all_test.go
@@ -1,6 +1,7 @@
 package aoc2025
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
@@ -11,15 +12,18 @@ import (
 func Test_all(t *testing.T) {
 	tests := []struct {
 		name     string
+		part     int
 		fn       func([]byte) (string, error)
 		expected string
 	}{
-		{"day1_sample", day1part1, "3"},
-		{"day1", day1part1, "964"},
+		{"day1_sample", 1, day1part1, "3"},
+		{"day1", 1, day1part1, "964"},
+		{"day1_sample", 2, day1part2, "6"},
+		{"day1", 2, day1part2, "5872"},
 	}
 
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+		t.Run(fmt.Sprintf("%s part %d", tt.name, tt.part), func(t *testing.T) {
 			input, err := os.ReadFile("./inputs/" + tt.name + ".txt")
 			require.NoError(t, err)
 			result, err := tt.fn(input)
--- day1.go
+++ day1.go
@@ -41,3 +41,39 @@ func day1part1(input []byte) (string, error) {
 
 	return strconv.Itoa(count_hit_zero), nil
 }
+
+func day1part2(input []byte) (string, error) {
+	pointer := 50
+	count_past_zero := 0
+
+	for line := range strings.Lines(string(input)) {
+		line = strings.TrimSpace(line)
+		direction := line[:1]
+		count, err := strconv.Atoi(line[1:])
+		if err != nil {
+			return "", err
+		}
+
+		op := map[string]func(int) int{
+			"L": func(i int) int { return i - 1 },
+			"R": func(i int) int { return i + 1 },
+		}[direction]
+
+		for range count {
+			pointer = op(pointer)
+			switch pointer {
+			case -1:
+				pointer = 99
+			case 100:
+				pointer = 0
+			}
+
+			if pointer == 0 {
+				count_past_zero += 1
+			}
+		}
+
+	}
+
+	return strconv.Itoa(count_past_zero), nil
+}
