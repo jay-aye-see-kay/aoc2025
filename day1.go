98c066cbd84dd21291f0ef2bad0cf1f2e26dd0f1
Jack Rose <user@jackrose.co.nz>
2025-12-02 10:09:34 +1100

    refactor: merge day1 approaches

--- day1.go
+++ day1.go
@@ -1,57 +1,38 @@
 package aoc2025
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 )
 
 func day1part1(input []byte) (string, error) {
-	pointer := 50
-	count_hit_zero := 0
-
-	for line := range strings.Lines(string(input)) {
-		line = strings.TrimSpace(line)
-		direction := line[:1]
-		count, err := strconv.Atoi(line[1:])
-		if err != nil {
-			return "", err
-		}
-
-		switch direction {
-		case "L":
-			pointer -= count
-		case "R":
-			pointer += count
-		default:
-			return "", fmt.Errorf("unknown direction: %s", direction)
-		}
-
-		for pointer < 0 {
-			pointer += 100
-		}
-		for pointer > 99 {
-			pointer -= 100
-		}
-
-		if pointer == 0 {
-			count_hit_zero += 1
-		}
+	zero_stops, _, err := _day1(input)
+	if err != nil {
+		return "", err
 	}
+	return strconv.Itoa(zero_stops), nil
 
-	return strconv.Itoa(count_hit_zero), nil
 }
 
 func day1part2(input []byte) (string, error) {
+	_, zero_passes, err := _day1(input)
+	if err != nil {
+		return "", err
+	}
+	return strconv.Itoa(zero_passes), nil
+}
+
+func _day1(input []byte) (int, int, error) {
 	pointer := 50
-	count_past_zero := 0
+	zero_stops := 0
+	zero_passes := 0
 
 	for line := range strings.Lines(string(input)) {
 		line = strings.TrimSpace(line)
 		direction := line[:1]
 		count, err := strconv.Atoi(line[1:])
 		if err != nil {
-			return "", err
+			return 0, 0, err
 		}
 
 		op := map[string]func(int) int{
@@ -69,11 +50,14 @@ func day1part2(input []byte) (string, error) {
 			}
 
 			if pointer == 0 {
-				count_past_zero += 1
+				zero_passes += 1
 			}
 		}
 
+		if pointer == 0 {
+			zero_stops += 1
+		}
 	}
 
-	return strconv.Itoa(count_past_zero), nil
+	return zero_stops, zero_passes, nil
 }
